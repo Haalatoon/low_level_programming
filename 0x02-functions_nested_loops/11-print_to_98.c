3e3f80804882fb7f3104f507bd30cb092afd5bb3
Haalatoon <rhilane.hala@gmail.com>
2023-09-25 09:42:23 -0700

    Task 11. 98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98 - prints numbers from n to 98
+ * @n: number to start counting from
+ *
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n >= 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
+		printf("\n");
+	}
+	else
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
+		printf("\n");
+	}
+}

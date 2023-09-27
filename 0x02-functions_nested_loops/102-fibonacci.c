b176e49ccf1230ae45eab20e735ccef5ea5addb8
Haalatoon <rhilane.hala@gmail.com>
2023-09-27 07:54:42 -0700

    Task 14. The first 50 Fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i, sum;
+
+	sum = 1;
+	i = 0;
+	do {
+		sum += i;
+		printf("%d", sum);
+		if (i < 49)
+		{
+			printf(',');
+		}
+		i++;
+	} while (i < 50)
+	printf('\n');
+	return (0);
+}
+
+
+

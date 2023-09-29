c94ff72eaf33572da74c106f75702f721a2eaf62
Haalatoon <rhilane.hala@gmail.com>
2023-09-29 16:03:02 -0700

    Task 11. The prime factors

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -5,47 +5,30 @@
  * Return: Always 0
  */
 
-int prime(int p);
-
 int main(void)
 {
-	long int i, n;
+	long int i, j, p;
+	int k;
 
-	for (i = 3; i < 612852475143; i++)
+
+	k = 0;
+	for (i = 2; i < 612852475143; i++)
 	{
-		if ((612852475143 % i) == 0 && prime(i) == 0)
+		if (612852475143 % i == 0)
 		{
-			n = i;
+			for (j = 2; j < i; j++)
+			{
+				if (i % j == 0)
+				{
+					k += 1;
+				}
+			}
+			if (k == 0)
+			{
+				p = i;
+			}
 		}
 	}
-	printf("%ld\n", n);
+	printf("%ld\n", p);
 	return (0);
-}
-
-/**
- * prime - determines if a number is primary or not
- * @p: number to enter
- *
- * Return: 1 if primary, 0 otherwise
- */
-int prime(int p)
-{
-	int i, count;
-
-	count = 0;
-	for (i = 2; i < p - 1; i++)
-	{
-		if (p % i == 0)
-		{
-			count += 1;
-		}
-	}
-	if (count == 0)
-	{
-		return (0);
-	}
-	else
-	{
-		return (1);
-	}
 }

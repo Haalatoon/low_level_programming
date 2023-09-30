56cc3c0e99a05ed2a6de28cf1a44238dd4dc380f
Haalatoon <rhilane.hala@gmail.com>
2023-09-30 03:51:31 -0700

    Task 2. This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: string to calculate its length
+ *
+ * Return: the length
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}

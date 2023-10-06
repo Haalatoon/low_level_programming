898059667a7f01b51918674aa3840bcd76be2ac5
Haalatoon <rhilane.hala@gmail.com>
2023-10-06 09:46:30 -0700

    Task 0. strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,11 +4,21 @@
  * @dest: first string
  * @src: second string
  *
- * Return: resulting string dest
+ * Return: resulting string
  */
 
 char *_strcat(char *dest, char *src)
 {
-	*dest = *dest + *src;
+	int i = 0, j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+	dest[i] = '\0';
 	return (dest);
 }
